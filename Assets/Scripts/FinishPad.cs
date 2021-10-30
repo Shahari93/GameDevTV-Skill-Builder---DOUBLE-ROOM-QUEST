fcc6dabcf32d16ec452288992834744742f06e20
Shahari93 <50714305+Shahari93@users.noreply.github.com>
2021-10-30 16:51:29 +0300

    First commit
    All assets + First task:
    Adjust the Camera as needed and make some of the top-level game objects transparent to help the player effectively see through the top level down to the bottom level as needed.

---
+++ Assets/Scripts/FinishPad.cs
@@ -0,0 +1,28 @@
+﻿// GameDev.tv Challenge Club. Got questions or want to share your nifty solution?
+// Head over to - http://community.gamedev.tv 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FinishPad : MonoBehaviour
+{
+    private GameHandler gameHandler;
+
+    private void Start()
+    {
+        //Challenge 3: 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerMovement>())
+        {
+            //This next line of code will console log an error until Challenge 3 is complete.
+            gameHandler.RemovePlayerCubeFromList(other.gameObject.GetComponent<PlayerMovement>());
+            Destroy(other.gameObject);
+            //Challenge 4:  
+        }
+    }
+}
